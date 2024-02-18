87e53922a6d023c6352ec79667fda755379d7048
kiruushinde <ks0120291@gmail.com>
2024-02-18 08:35:01 +0530

    Done with installation of VS Code and MingW

---
+++ Day2/day2.cpp
@@ -0,0 +1,29 @@
+
+
+/*
+    1. installation of vs code - go to official website
+    2. mingW - take it from drive and install
+    
+    settings to enable 
+    1. zoom --> zoom on mousewheel
+    2. install extensions C, C++, CodeRunner #️⃣ imp
+
+    3. key board shortcuts --> copyline down, move up-down
+    
+    try multiple cursor 🤓 (press alt and click mouse)
+
+    4. settings
+        - format on save
+        - run in terminal
+        - zoom
+        - word wrap
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello";
+return 0;
+}
