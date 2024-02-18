1142fa0fbae29ae449cad53c699015c39537d26b
kiruushinde <ks0120291@gmail.com>
2024-02-18 08:32:53 +0530

    Just Introducing O(1) Programming Club it is..

---
+++ Day01/Day1.cpp
@@ -0,0 +1,29 @@
+/*
+    - Introduction to club
+
+    - why programming ?
+
+    - what we wiil be dealing with ?
+    - how ?
+    - platforms and hands on ?
+    - what do you want ?
+
+    coder of week
+    coder of week
+
+    - games
+    1. guess the number
+    2. snake water gun
+
+    - vs code
+    -
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
